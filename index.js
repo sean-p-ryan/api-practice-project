2cb93b910d90547a84784363205f1858a6430e4a
Sean Ryan <ryan.sean@acmhq.acm.org>
2019-08-27 18:12:11 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+ express = require('express');
+ const app = express();
+ app.listen(3000, () => console.log('listening at 3000'))
+ app.use(express.static('public'));
+ app.use(express.json({ limit: '1mb' }));
+
+ app.post('/api', (request, response) => {
+     console.log('I got a request')
+     console.log(request.body);
+     response.json({
+         status: "success",
+         latitude: 'lat',
+         longitude: 'lon'
+     });
+ });
