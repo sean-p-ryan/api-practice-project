405979d3fccccaac1ab64c67dc41e5098a3645e0
Sean Ryan <ryan.sean@acmhq.acm.org>
2019-08-28 17:52:15 -0400

    added API call to get weather in client's location

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
- express = require('express');
+const express = require('express');
+const Datastore = require('nedb');
+const fetch = require('node-fetch');
+
  const app = express();
  app.listen(3000, () => console.log('listening at 3000'))
  app.use(express.static('public'));
  app.use(express.json({ limit: '1mb' }));
 
+ const database = new Datastore('database.db');
+ database.loadDatabase();
+
  app.post('/api', (request, response) => {
-     console.log('I got a request')
-     console.log(request.body);
      response.json({
          status: "success",
          latitude: 'lat',
          longitude: 'lon'
      });
  });
+
+app.get('/weather/:latlon', async (request, response) => {
+ console.log(request.params)
+ const latlon = request.params.latlon.split(',');
+ console.log(latlon);
+ const lat = latlon[0];
+ const lon = latlon[1];
+ console.log(lat, lon);
+ const api_url = `https://api.darksky.net/forecast/b93ee3c59f5bdc3b2897e35e2901ad65/${lat},${lon}` 
+ const fetch_response = await fetch(api_url);
+ const json = await fetch_response.json();
+ response.json(json)
+});
